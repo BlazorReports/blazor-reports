6ca3b6f007faf60582a694de02bc4ee6f968af6b
Elvis Nieves Miranda <elvis.nievesmiranda@gmail.com>
2023-05-29 18:18:31 -0400

    feat: add base razor class and base razor template to simplify report development

--- src/BlazorReports/Services/IReportService.cs
+++ src/BlazorReports/Services/IReportService.cs
@@ -6,7 +6,7 @@ namespace BlazorReports.Services;
 public interface IReportService
 {
   Task<MemoryStream> GenerateReport<T, TD>(TD data) where T : ComponentBase where TD : class;
-  Task<MemoryStream> GenerateReport<T>(BlazorReport blazorReport, T data) where T : class;
+  Task<MemoryStream> GenerateReport<T>(BlazorReport blazorReport, T? data) where T : class;
   Task<MemoryStream> GenerateReport(BlazorReport blazorReport);
   BlazorReport? GetReportByName(string name);
 }
