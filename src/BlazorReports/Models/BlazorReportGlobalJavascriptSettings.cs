8acd01a966709b6d78927ea92dcff87eb341e004
Andres <andresitomanuel@gmail.com>
2025-03-22 19:14:25 -0400

    Code clean up

--- src/BlazorReports/Models/BlazorReportGlobalJavascriptSettings.cs
+++ src/BlazorReports/Models/BlazorReportGlobalJavascriptSettings.cs
@@ -1,6 +1,7 @@
 namespace BlazorReports.Models;
+
 /// <summary>
-/// 
+///
 /// </summary>
 public class BlazorReportGlobalJavascriptSettings
 {
@@ -8,11 +9,13 @@ public class BlazorReportGlobalJavascriptSettings
   /// The signal that the report is ready
   /// </summary>
   public string ReportIsReadySignal { get; set; } = "reportIsReady";
+
   /// <summary>
   /// Decides if the report should wait for the complete signal in the javascript.
   /// (Default: false)
   /// </summary>
   public bool WaitForJavascriptCompletedSignal { get; set; }
+
   /// <summary>
   ///  The amount of time a reports javascript can take until it is considered to have timed out
   ///  (Default: null)
